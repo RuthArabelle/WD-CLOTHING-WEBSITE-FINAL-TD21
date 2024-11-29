ccb83a406a686fecf8d6154a5e81f71a239a7394
RuthArabelle <202310983@fit.edu.ph>
2024-11-29 21:40:55 +0800

    iyak

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,1 @@
 
-const slider = document.querySelector('.slider');
-const slides = document.querySelectorAll('.slide');
-const btnLeft = document.querySelector('.slider-btn.left');
-const btnRight = document.querySelector('.slider-btn.right');
-
-let currentIndex = 1;
-
-btnLeft.addEventListener('click', () => {
-    currentIndex = (currentIndex === 0) ? slides.length - 1 : currentIndex - 1;
-    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
-});
-
-btnRight.addEventListener('click', () => {
-    currentIndex = (currentIndex === slides.length - 1) ? 0 : currentIndex + 1;
-    slider.style.transform = `translateX(-${currentIndex * 100}%)`;
-});
